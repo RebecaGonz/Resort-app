d7ddd7164947871e70f48fb25aa3c0c3e27fe9ee
RebecaGonz <RebecaGonz@users.noreply.github.com>
2021-01-26 01:15:27 -0800

    Component Main
    Add NavBar, and Hero section

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,7 @@
+import { Link } from '@reach/router';
 import React, { useState } from 'react'
 import styles from './index.module.css'
+import { FaAlignRight } from "react-icons/fa"
 
 function NavBar() {
     const [isOpen, setOpen] = useState(false);
@@ -7,12 +9,30 @@ function NavBar() {
     const handleToggle = () => {
         setOpen(!isOpen);
     }
-
-    console.log(styles)
+    console.log(isOpen);
 
     return (
-        <div data-text="123" className={styles.navbar}>
-            <button type="button" className='nav-btn' onClick={handleToggle}></button>
+        <div>
+        <nav  className={styles.navbar}>
+            <div className={styles.navCenter}>
+                <div className={styles.navHeader}>
+                    <Link to="/home">
+                        <img src='#' alt="logofromhotel" />
+                    </Link>
+                    <button type="button" className={styles.navBtn} onClick={handleToggle}>
+                        <FaAlignRight className={styles.navIcon} />
+                    </button>
+                </div>
+                <ul className={isOpen? styles.navShow: styles.navLinks }>
+                    <li>
+                        <Link to = "/home">Home</Link>
+                    </li>
+                    <li>
+                        <Link to = "/rooms">Rooms</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
         </div>
     )
 }
--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from '../components/NavBar'
+import Hero from '../components/Hero'
+import {FaCocktail, FaHiking, FaShuttleVan, FaBeer} from 'react-icons/fa'
+import styles from '../components/index.module.css'
 
 function Main() {
+    const [services, setServices]= useState({
+        services:[
+            {
+                icon: <FaCocktail />,
+                title: "free cocktails",
+                info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+            },
+            {
+                icon: <FaHiking />,
+                title: "free cocktails",
+                info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+            },
+            {
+                icon: <FaShuttleVan />,
+                title: "free cocktails",
+                info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+            },
+            {
+                icon: <FaBeer/>,
+                title: "free cocktails",
+                info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+            }
+        ]
+    })
+
     return (
         <div>
-            <NavBar />
+            <NavBar/>
+            <Hero/>
+            <div>
+                <h1>Services</h1>
+                <div className={styles.heroLine}></div>
+                <div>
+                    <div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
