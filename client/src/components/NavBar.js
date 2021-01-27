c7fb9d8b2ef19cad7989a2007887660aad38db59
Retchiele <rldatuin@gmail.com>
2021-01-27 11:41:06 -0800

    login

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,6 +2,7 @@ import { Link } from '@reach/router';
 import React, { useState } from 'react'
 import styles from './index.module.css'
 import { FaAlignRight } from "react-icons/fa"
+import userIcon from './images/userIcon.png'
 
 function NavBar() {
     const [isOpen, setOpen] = useState(false);
@@ -13,26 +14,29 @@ function NavBar() {
 
     return (
         <div>
-        <nav  className={styles.navbar}>
-            <div className={styles.navCenter}>
-                <div className={styles.navHeader}>
-                    <Link to="/home">
-                        <img src='#' alt="logofromhotel" />
-                    </Link>
-                    <button type="button" className={styles.navBtn} onClick={handleToggle}>
-                        <FaAlignRight className={styles.navIcon} />
-                    </button>
+            <nav  className={styles.navbar}>
+                <div className={styles.navCenter}>
+                    <div className={styles.navHeader}>
+                        <Link to="/home">
+                            <img src='#' alt="logofromhotel" />
+                        </Link>
+                        <button type="button" className={styles.navBtn} onClick={handleToggle}>
+                            <FaAlignRight className={styles.navIcon} />
+                        </button>
+                    </div>
+                    <ul className={isOpen? styles.navShow: styles.navLinks }>
+                        <li>
+                            <Link to = "/home">Home</Link>
+                        </li>
+                        <li>
+                            <Link to = "/rooms">Rooms</Link>
+                        </li>
+                        <li>
+                            <Link to = "/register"><img src={userIcon} height="25px" width="25px"></img></Link>
+                        </li>
+                    </ul>
                 </div>
-                <ul className={isOpen? styles.navShow: styles.navLinks }>
-                    <li>
-                        <Link to = "/home">Home</Link>
-                    </li>
-                    <li>
-                        <Link to = "/rooms">Rooms</Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+            </nav>
         </div>
     )
 }
