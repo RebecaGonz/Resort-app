53f9398bc51ab654b6ecabc15f039a3266249c39
devNicku <stsnicky@gmail.com>
2021-01-28 21:16:41 -0800

    added new component view reservation

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-import Main from './views/Main'
-import Rooms from './views/Rooms'
-import SingleRoom from './views/SingleRoom'
-import Error from './views/Error'
+import Main from './views/Main';
+import Rooms from './views/Rooms';
+import SingleRoom from './views/SingleRoom';
+import Error from './views/Error';
 import { Router, Redirect } from '@reach/router';
-import Register from './components/Register'
+import Register from './components/Register';
+import ViewReservation from './views/ViewReservation';
 
 function App() {
     return (
@@ -17,6 +18,7 @@ function App() {
                 <SingleRoom path="/home/singleRooms/:id" />
                 <Error path="/home/error" />
                 <Register path="/register" />
+                <ViewReservation path="/reservation" />
             </Router>
         </div>
     );
